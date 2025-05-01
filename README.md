9358338cac060a55410d9d87d6cee2635d1ac606
Haranmes <soliman.ramez@proton.me>
2025-05-01 19:30:43 +0200

    Added README.md text

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ramez-uefi-os
+This is my first (serious) Project using Rust
+---
+
+## The Bootloader
+- Created with `r_uefi` and the rust nightly feature `uefi_std`
+- UEFI bootstrapping in Rust
+- Configuration tables and `GUIDs` strictly from `r_efi::system`
+- Console I/O through own `uefi_println` makro
+- Memory map handling
+- Boot service interaction with `system_table`
+- Image handle for future kernel
+
+## The Kernel (WIP)
--- src/cfg_table.rs
+++ src/cfg_table.rs
@@ -1,9 +1,8 @@
 use core::format_args;
 
-use crate::cfg_table_guid;
+use crate::makros;
 use r_efi::base::Guid;
 
-use crate::makros;
 #[derive(Debug)]
 pub struct CfgTableType(r_efi::efi::Guid);
 
@@ -18,6 +17,8 @@ impl From<Guid> for CfgTableType {
  * This implements a Display trait that will output the human-readable name for any recognized GUIDs
  * while falling back to the Display implementation in uefi::Guid, for unrecognized ones.
  */
+
+// i am actually to lazy to create a Bitmap to make this block better, this works just fine
 impl core::fmt::Display for CfgTableType {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
         match self.0 {
@@ -30,7 +31,20 @@ impl core::fmt::Display for CfgTableType {
             r_efi::system::SMBIOS3_TABLE_GUID => f.write_str("SMBIOS 3.0 Table"),
             r_efi::system::SMBIOS_TABLE_GUID => f.write_str("SMBIOS Table"),
             r_efi::system::DTB_TABLE_GUID => f.write_str("Device Tree Blob Table"),
-            _ => f.write_str("Unknown GUID"),
+            r_efi::system::SAL_SYSTEM_TABLE_GUID => f.write_str("Sal System"),
+            r_efi::system::JSON_CONFIG_DATA_TABLE_GUID => f.write_str("JSON Configuration Table"),
+            r_efi::system::JSON_CAPSULE_DATA_TABLE_GUID => f.write_str("JSON Capsule Table"),
+            r_efi::system::JSON_CAPSULE_RESULT_TABLE_GUID => {
+                f.write_str("JSON Capsule Result Table")
+            }
+            r_efi::system::CONFORMANCE_PROFILES_TABLE_GUID => {
+                f.write_str("EFI specification conformance")
+            }
+            r_efi::system::CONFORMANCE_PROFILES_UEFI_SPEC_GUID => {
+                f.write_str("Conformance Profiles")
+            }
+            r_efi::system::HARDWARE_ERROR_VARIABLE_GUID => f.write_str("Hardware Error"),
+            _ => write!(f, "GUID: {:?}", self.0),
         }
     }
 }
