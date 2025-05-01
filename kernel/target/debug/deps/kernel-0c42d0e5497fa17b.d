b609fc6f227cca016930c9c52ea7dfb30f09a6f7
Haranmes <soliman.ramez@proton.me>
2025-05-01 23:50:54 +0200

    added kernel project

---
+++ kernel/target/debug/deps/kernel-0c42d0e5497fa17b.d
@@ -0,0 +1,5 @@
+/home/rsoli/Rust_Projects/ramOS/kernel/target/debug/deps/kernel-0c42d0e5497fa17b.d: src/main.rs
+
+/home/rsoli/Rust_Projects/ramOS/kernel/target/debug/deps/libkernel-0c42d0e5497fa17b.rmeta: src/main.rs
+
+src/main.rs:
