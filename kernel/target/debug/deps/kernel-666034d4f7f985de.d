c35b5408dd330da0f43718f99bc13ce40172599d
Haranmes <soliman.ramez@proton.me>
2025-05-02 19:07:13 +0200

    cleanup

--- kernel/target/debug/deps/kernel-666034d4f7f985de.d
+++
@@ -1,5 +0,0 @@
-/home/rsoli/Rust_Projects/ramOS/kernel/target/debug/deps/kernel-666034d4f7f985de.d: src/main.rs
-
-/home/rsoli/Rust_Projects/ramOS/kernel/target/debug/deps/libkernel-666034d4f7f985de.rmeta: src/main.rs
-
-src/main.rs:
